528742165e230c89a6fd44946deceef554f11fcd
Charu <charushila.jamnis@gmail.com>
2020-08-12 19:46:27 -0400

    Niesha and Charu Calculator assignment

--- CLICalculator.java
+++ CLICalculator.java
@@ -6,8 +6,54 @@
 public class CLICalculator {
 
 	public static void main(String[] args) {
-		
-
+				
+			Scanner input = new Scanner(System.in);
+			
+			System.out.println("Welcome to the CLI Calculator");
+			System.out.println("Operations: \n1:Addition \n2:Subtraction \n3:Multiplication \n4:Division");
+			System.out.println("\nChoose your operation: ");
+			int operation = input.nextInt();
+			System.out.println("\nFirst Number: ");
+			int number1 = input.nextInt();
+			System.out.println("Second Number: ");
+			int number2 = input.nextInt();
+			int result;
+				
+			//addition
+			if(operation == 1)
+			{
+				result = number1+number2;
+				System.out.println("Result: "+result);
+			}
+			//subtraction
+			else if(operation ==2)
+			{
+				result= number1-number2;
+				System.out.println("Result: "+result);
+			}
+			//multiplication
+			else if(operation==3)
+			{	
+				result = number1 * number2;
+				System.out.println("Result: "+result);
+			}
+			//division
+			else if(operation ==4)
+			{
+				if(number2==0)
+				{
+					System.out.println("Result: cannot be divide by 0");
+				}
+				else
+				{
+					result = number1/number2;
+					System.out.println("Result: "+result);
+				}
+			}
+			else
+			{
+				System.out.println("Please enter valid operation number.");
+			}
 	}
 
 }
